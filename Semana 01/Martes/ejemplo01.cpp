6b2e28596cc7a0ab139954b0aa3ff1a4b7c156fd
Angel <43154124+angel-utn@users.noreply.github.com>
2025-03-18 20:11:57 -0300

    Archivos de la Semana 01

---
+++ Semana 01/Martes/ejemplo01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int num1, num2, resultado;
+
+   cout << "Primer programa: Calculadora de suma" << endl << endl;
+
+   cout << "Ingresa el primer numero: ";
+   cin >> num1;
+   cout << "Ingresa el segundo numero: ";
+   cin >> num2;
+   cout << endl;
+   resultado = num1 + num2;
+   cout << "El resultado de la suma es: " << resultado;
+
+   cout << endl;
+
+   return 0;
+}
