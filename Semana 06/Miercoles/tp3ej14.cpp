75dcb90dda9de50a0aeb4b67bf9224345e1fc20a
Brian Lara <brian.lara@outlook.com.ar>
2025-04-23 21:17:57 -0300

    ejeicios

---
+++ Semana 06/Miercoles/tp3ej14.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+/***
+Dada una lista de 7 numeros enteros informar 
+ - cual es el primer
+ - el segundo, 
+ - el anteultimo 
+ - ultimo numero impar ingresado.
+   Ejemplo 8, 4, -5, 7, 9, 18, 5 se informa: Primer impar: -5, Segundo impar: 7, Anteultimo impar: 9 y Ultimo impar: 5.
+*/
+
+/**
+8
+4
+-5
+7
+9
+18
+5 
+*/
+
+int main() {
+   int numero, cantImpar, primero, segundo, anteultimo, ultimo;
+   
+   
+   cantImpar = 0;
+   /// ingreso 7 numeros
+   for(int i=1; i<=7; i++){
+      cout << "Ingrese numero:";
+      cin >> numero;
+      
+      /// verifico si el numero es impar
+      if(numero % 2 != 0){
+         cantImpar++;
+         
+         if(cantImpar == 1){
+            primero = numero;
+         }
+         
+         if(cantImpar == 2){
+            segundo = numero;
+         }
+         
+         anteultimo = ultimo;
+         ultimo = numero;
+      }
+   }
+   
+   if(cantImpar > 0){
+      cout << "El primero es: " << primero << endl;   
+   }
+   
+   if(cantImpar > 1){
+      cout << "El segundo es: " << segundo << endl;
+      cout << "El anteultimo es: " << anteultimo << endl;
+   }
+   
+   if(cantImpar > 0){
+      cout << "El ultmo es: " << ultimo << endl;
+   }
+      
+	
+	return 0;
+}
+
---
+++ Semana 06/Miercoles/tp4ej15.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+/***
+
+Hacer un programa para ingresar una lista de numeros 
+que finaliza cuando se ingresa un cero, informar los 
+2 mayores valores ingresados, aclarando cual es el
+maximo y cual el que le sigue.
+Ejemplo: 
+   10, 8, 12, 14, 3, 0 
+   el resultado sera 14 y 12.Ejemplo: 
+   14, 8, 12, 14, 3, 0 
+   el resultado sera 14 y 14.Ejemplo: 
+   -4, -8, -12, -20, -2, 0 
+   el resultado sera -2 y -4
+
+*/
+
+int main() {
+   int numero, primerMaximo, segundoMaximo;
+   
+   cout << "Ingrese numero: ";
+   cin >> numero;
+   
+   primerMaximo = 0; /// valor absurdo
+   segundoMaximo = 0;
+   
+   /// primer = 11
+   /// segundo = 10
+   while(numero != 0){
+      
+      if(primerMaximo == 0 || numero > primerMaximo){
+         segundoMaximo = primerMaximo;
+         primerMaximo = numero;
+      }
+      else{
+         if(segundoMaximo == 0 || numero > segundoMaximo){
+            segundoMaximo = numero;
+         }
+      }
+      
+      cout << "Ingrese numero: ";
+      cin >> numero;   
+   }
+      
+	
+	return 0;
+}
+
